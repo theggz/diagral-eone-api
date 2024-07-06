a53952b8ca5f0591e0381dd7c85b94df714fb18c
theggz <75008172+theggz@users.noreply.github.com>
2024-07-07 00:17:19 +0200

    Import first test class

---
+++ tests/api/test_api.py
@@ -0,0 +1,18 @@
+
+"""API test class."""
+
+from faker import Faker
+from diagral_eone.api.api import DiagralEOneApi
+
+class TestApi:
+    """API test."""
+    fake = Faker()
+    username = fake.user_name()
+    password = fake.password()
+
+    def test_api_init(self):
+        """Test initializer."""
+        diagral_api = DiagralEOneApi(self.username, self.password, None)
+        assert diagral_api.username == self.username
+        assert diagral_api.password == self.password
+    
